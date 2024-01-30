24f2c08028378459314b097267635fbdb75ad113
Tina Shen <shentina18@gmail.com>
2024-01-30 12:05:40 +1100

    adding more complex workflows

--- python_functions.py
+++ python_functions.py
@@ -8,3 +8,130 @@ def test_math_function(x,y):
 	
 def test_numpy_function(x,y):
 	return(np.add(x,y))
+
+def cell(filepath, resultspath):
+  
+# coding: utf-8
+
+# Image Analysis with Python - Solution for Batch Processing
+
+# The following is the script version of the tutorial's solution pipeline, where all the code
+# has been wrapped in a single function that can be called many times for many images.
+# Please refer to the jupyter notebooks ('image_analysis_tutorial[_solutions].ipynb') for 
+# more information, including detailed comments on every step.
+
+
+## Importing Modules & Packages
+    import csv
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import scipy.ndimage as ndi
+    from skimage.io import imread
+
+    img = imread(filepath)
+
+
+    ## Preprocessing
+
+    sigma = 3
+    img_smooth = ndi.gaussian_filter(img, sigma)
+
+
+    ## Adaptive Thresholding
+
+    i = 31
+    SE = (np.mgrid[:i,:i][0] - np.floor(i/2))**2 + (np.mgrid[:i,:i][1] - np.floor(i/2))**2 <= np.floor(i/2)**2
+
+    from skimage.filters import rank 
+    bg = rank.mean(img_smooth, footprint=SE)
+
+    mem = img_smooth > bg
+
+
+    ## Improving Masks with Binary Morphology
+
+    mem_holefilled = ~ndi.binary_fill_holes(~mem) # Short form
+
+    i = 15
+    SE = (np.mgrid[:i,:i][0] - np.floor(i/2))**2 + (np.mgrid[:i,:i][1] - np.floor(i/2))**2 <= np.floor(i/2)**2
+
+    pad_size = i+1
+    mem_padded = np.pad(mem_holefilled, pad_size, mode='reflect')
+    mem_final = ndi.binary_closing(mem_padded, structure=SE)
+    mem_final = mem_final[pad_size:-pad_size, pad_size:-pad_size]
+
+
+    ## Cell Segmentation by Seeding & Expansion
+
+    ### Seeding by Distance Transform
+
+    dist_trans = ndi.distance_transform_edt(~mem_final)
+    dist_trans_smooth = ndi.gaussian_filter(dist_trans, sigma=5)
+
+    from skimage.feature import peak_local_max
+    seed_coords = peak_local_max(dist_trans_smooth, min_distance=10)
+    seeds = np.zeros_like(dist_trans_smooth, dtype=bool)
+    seeds[tuple(seed_coords.T)] = True
+
+    seeds_labeled = ndi.label(seeds)[0]
+
+    ### Expansion by Watershed
+
+    from skimage.segmentation import watershed
+    ws = watershed(img_smooth, seeds_labeled)
+
+
+    ## Postprocessing: Removing Cells at the Image Border
+
+    border_mask = np.zeros(ws.shape, dtype=bool)
+    border_mask = ndi.binary_dilation(border_mask, border_value=1)
+
+    clean_ws = np.copy(ws)
+
+    for cell_ID in np.unique(ws):
+        cell_mask = ws==cell_ID
+        cell_border_overlap = np.logical_and(cell_mask, border_mask)
+        total_overlap_pixels = np.sum(cell_border_overlap)
+        if total_overlap_pixels > 0: 
+            clean_ws[cell_mask] = 0
+
+    for new_ID, cell_ID in enumerate(np.unique(clean_ws)[1:]): 
+        clean_ws[clean_ws==cell_ID] = new_ID+1
+
+
+    ## Identifying Cell Edges
+
+    edges = np.zeros_like(clean_ws)
+
+    for cell_ID in np.unique(clean_ws)[1:]:
+        cell_mask = clean_ws==cell_ID
+        eroded_cell_mask = ndi.binary_erosion(cell_mask, iterations=1)
+        edge_mask = np.logical_xor(cell_mask, eroded_cell_mask)
+        edges[edge_mask] = cell_ID
+
+
+    ## Extracting Quantitative Measurements
+
+    results = {"cell_id"      : [],
+               "int_mean"     : [],
+               "int_mem_mean" : [],
+               "cell_area"    : [],
+               "cell_edge"    : []}
+
+    with open(resultspath, 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Write the header
+        writer.writerow(['cell_id', 'int_mean', 'int_mem_mean', 'cell_area', 'cell_edge'])
+        
+        # Write the data rows
+        for cell_id in np.unique(clean_ws)[1:]:
+            cell_mask = clean_ws==cell_id
+            edge_mask = edges==cell_id
+            int_mean = np.mean(img[cell_mask])
+            int_mem_mean = np.mean(img[edge_mask])
+            cell_area = np.sum(cell_mask)
+            cell_edge = np.sum(edge_mask)
+            writer.writerow([cell_id, int_mean, int_mem_mean, cell_area, cell_edge])
+    return(resultspath)
+        
+
